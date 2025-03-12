a00b7ef18117e10b3ef6cb7ef2512bfde6b29d95
Tristan Qesen Sanchez Mayen <tristanqsm@MacBook-Air-de-Tristan.local>
2025-03-12 13:31:22 -0600

    Agregando archivos base

---
+++ Lexer/GUI.py
@@ -0,0 +1,62 @@
+import tkinter as tk
+from tkinter import filedialog, messagebox
+
+class CodeAnalyzerApp:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("LEXER")
+        self.root.geometry("800x400")
+        
+        self.selected_language = 'C'
+        
+        # Crear un PanedWindow para dividir la ventana en dos paneles
+        self.paned_window = tk.PanedWindow(root, orient=tk.HORIZONTAL)
+        self.paned_window.pack(fill=tk.BOTH, expand=True)
+        
+        # Panel izquierdo para la entrada de texto
+        self.left_panel = tk.Frame(self.paned_window)
+        self.paned_window.add(self.left_panel)
+        
+        # Área de texto para la entrada
+        self.text_area_input = tk.Text(self.left_panel, height=20)
+        self.text_area_input.pack(fill=tk.BOTH, expand=True, padx=10, pady=10)
+        
+        # Botón para cargar archivo
+        tk.Button(self.left_panel, text="Cargar Archivo", command=self.load_file).pack()
+        
+        # Panel derecho para la salida de texto
+        self.right_panel = tk.Frame(self.paned_window)
+        self.paned_window.add(self.right_panel)
+        
+        # Área de texto para la salida
+        self.text_area_output = tk.Text(self.right_panel, height=20)
+        self.text_area_output.pack(fill=tk.BOTH, expand=True, padx=10, pady=10)
+        
+        # Botón para analizar
+        tk.Button(self.left_panel, text="Analizar", command=self.analyze).pack()
+    
+    def load_file(self):
+        ext = {"C": "c"}
+        selected_ext = ext[self.selected_language]
+        file_path = filedialog.askopenfilename(filetypes=[("Archivos de Código", f"*.{selected_ext}")])
+        
+        if file_path:
+            with open(file_path, "r", encoding="utf-8") as file:
+                self.text_area_input.delete("1.0", tk.END)
+                self.text_area_input.insert(tk.END, file.read())
+    
+    def analyze(self):
+        code_content = self.text_area_input.get("1.0", tk.END).strip()
+        if not code_content:
+            messagebox.showwarning("Advertencia", "No hay código para analizar")
+        else:
+            messagebox.showinfo("Análisis", f"Analizando código en {self.selected_language}")
+            # Aquí puedes agregar análisis adicional del código ingresado.
+            # Por ejemplo, podrías mostrar el resultado en el área de texto de salida
+            self.text_area_output.delete("1.0", tk.END)
+            self.text_area_output.insert(tk.END, "Resultado del análisis...")
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = CodeAnalyzerApp(root)
+    root.mainloop()
---
+++ Lexer/LEX_C.py
@@ -0,0 +1,108 @@
+import ply.lex as lex
+
+# Definicion de palabras clave (keywords)
+keywords = {
+    'if': 'IF',
+    'else': 'ELSE',
+    'while': 'WHILE',
+    'return': 'RETURN',
+    'int': 'INT',
+    'float': 'FLOAT',
+    'char': 'CHAR',
+    'void': 'VOID'
+}
+
+# Definicion de los tokens
+tokens = [
+    'ID',          # Identificadores (variables, funciones)
+    'NUMBER',      # Números (enteros y flotantes)
+    'TEXT',         # Texto entre comillas 
+    'MAS',        # +
+    'MENOS',       # -
+    'MULTIPLICACION',    # *
+    'DIVISION',      # /
+    'ASIGNACION',      # =
+    'IGUALDAD',          # ==
+    'MENORQUE',          # <
+    'MAYORQUE',          # >
+    'PARENIZQ',      # (
+    'PARENDER',      # )
+    'CORCHIZQ',      # {
+    'CORCHDER',      # }
+    'PUNTOCOMA',   # ;
+    'COMA'        # ,
+] + list(keywords.values())  # Agregamos las palabras clave a los tokens
+
+# Reglas de expresiones regulares para tokens simples
+t_MAS               = r'\+'
+t_MENOS             = r'-'
+t_MULTIPLICACION    = r'\*'
+t_DIVISION          = r'/'
+t_ASIGNACION        = r'='
+t_IGUALDAD          = r'=='
+t_MENORQUE          = r'<'
+t_MAYORQUE          = r'>'
+t_PARENIZQ          = r'\('
+t_PARENDER          = r'\)'
+t_CORCHIZQ          = r'\{'
+t_CORCHDER          = r'\}'
+t_PUNTOCOMA         = r';'
+t_COMA              = r','
+
+# Para ignorar los espacios y tabulaciones
+t_ignore = ' \t'
+
+# Expresión regular para números (enteros y flotantes)
+def t_NUMBER(t):
+    r'\d+(\.\d+)?'
+    t.value = float(t.value) if '.' in t.value else int(t.value)
+    return t
+
+# Expresión regular para literales (enteros y flotantes)
+def t_TEXT(t):
+    r'"([^"\\]|\\.)*"'
+    t.type = keywords.get(t.value, 'TEXT') 
+    return t
+
+# Expresión regular para identificadores y palabras clave
+def t_ID(t):
+    r'[a-zA-Z_][a-zA-Z_0-9]*'
+    t.type = keywords.get(t.value, 'ID')  # Si es keyword, cambia el tipo
+    return t
+
+# Manejo de errores en caracteres desconocidos
+def t_error(t):
+    #print(f"Carácter ilegal '{t.value[0]}' en la posición {t.lexpos}")
+    t.lexer.skip(1)
+
+# Creamos el lexer
+lexer = lex.lex()
+
+# Código de prueba
+codigoPrueba = """
+int suma(a,b){
+    int suma = a + b;
+    return suma;
+}
+
+int main(){
+    int suma;
+    int a = 10;
+    int b = 20; 
+    suma = suma(a,b);
+    print("El total es: %d",suma);
+}
+"""
+
+# Metemos el texto (codigoPrueba) al lexer
+lexer.input(codigoPrueba)
+
+# Contamos los tokens
+TotalTokens = 0
+for tok in lexer:
+    print(tok)
+    TotalTokens = TotalTokens+1
+
+# Imprimimos el numero total de tokens
+print("Total de tokes = ", TotalTokens)
+
